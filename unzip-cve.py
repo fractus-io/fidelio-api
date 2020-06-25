6dd50b17403d6defa192499a6b180b07780429de
isakal <sakyYT@gmail.com>
2020-06-25 13:54:05 +0200

    Make unzip-cve modular by making it a function

--- unzip-cve.py
+++ unzip-cve.py
@@ -6,25 +6,6 @@ import json
 
 files = [f for f in listdir("nvd/") if isfile(join("nvd/", f))]
 files.sort()
-# print(files)
-
-
-# for file in files:
-#     print(join("nvd/", file))
-
-#     archive = zipfile.ZipFile(join("nvd/", file), 'r')
-
-#     jsonfile = archive.open(archive.namelist()[0])
-#     cve_dict = json.loads(jsonfile.read())
-
-#     # print("CVE_data_timestamp: " + str(cve_dict['CVE_data_timestamp']))
-#     # print("CVE_data_version: " + str(cve_dict['CVE_data_version']))
-#     # print("CVE_data_format: " + str(cve_dict['CVE_data_format']))
-#     print("CVE_data_numberOfCVEs: " + str(cve_dict['CVE_data_numberOfCVEs']))
-#     num += int(cve_dict.get("CVE_data_numberOfCVEs"))
-#     # print("CVE_data_type: " + str(cve_dict['CVE_data_type']))
-
-#     jsonfile.close()
 
 file = zipfile.ZipFile("nvd/nvdcve-1.1-2020.json.zip", "r")
 json_file = file.open(file.namelist()[0])
@@ -42,50 +23,85 @@ cve = data.get("CVE_Items")[0]
 # print(cve)
 # print(json.dumps(cve, indent=4))
 
-for index, report in enumerate(data["CVE_Items"]):
 
-    try:
+def extract_data_from_zip(target_file):
+    """unzip the file, parse the data and return a list of CVEs"""
 
-        cve_id = report.get("cve").get("CVE_data_meta").get("ID")
-        last_mod_date = datetime.strptime(report.get("lastModifiedDate"), "%Y-%m-%dT%H:%MZ")
-        pub_date = datetime.strptime(report.get("publishedDate"), "%Y-%m-%dT%H:%MZ")
-        summary = report.get("cve").get("description").get("description_data")[0].get("value")
-        impact = report.get("impact")
+    file = zipfile.ZipFile(target_file, "r")
+    json_file = file.open(file.namelist()[0])
 
-        if "REJECT" in summary:
+    data = json.loads(json_file.read())
+
+    cves = []
+
+    for index, report in enumerate(data["CVE_Items"]):
+
+        try:
+
+            cve_id = report.get("cve").get("CVE_data_meta").get("ID")
+            last_mod_date = datetime.strptime(report.get("lastModifiedDate"), "%Y-%m-%dT%H:%MZ")
+            pub_date = datetime.strptime(report.get("publishedDate"), "%Y-%m-%dT%H:%MZ")
+            summary = report.get("cve").get("description").get("description_data")[0].get("value")
+            impact = report.get("impact")
+
+            if "REJECT" in summary:
+                continue
+
+            if impact != {}:
+                baseMetricV2 = impact.get("baseMetricV2")
+
+                cvss_base = baseMetricV2.get("cvssV2").get("baseScore")
+                cvss_impact = baseMetricV2.get("impactScore")
+                cvss_exploit = baseMetricV2.get("exploitabilityScore")
+                cvss_access_vector = baseMetricV2.get("cvssV2").get("accessVector")
+                cvss_access_complexity = baseMetricV2.get("cvssV2").get("accessComplexity")
+                cvss_access_authentication = baseMetricV2.get("cvssV2").get("authentication")
+                cvss_confidentiality_impact = baseMetricV2.get("cvssV2").get("confidentialityImpact")
+                cvss_integrity_impact = baseMetricV2.get("cvssV2").get("integrityImpact")
+                cvss_availability_impact = baseMetricV2.get("cvssV2").get("availabilityImpact")
+                cvss_vector = baseMetricV2.get("cvssV2").get("vectorString")
+                cwe_id = report.get("cve").get("problemtype").get("problemtype_data")[0].get("description")[0].get("value")
+            else:
+
+                cvss_base = None
+                cvss_impact = None
+                cvss_exploit = None
+                cvss_access_vector = None
+                cvss_access_complexity = None
+                cvss_access_authentication = None
+                cvss_confidentiality_impact = None
+                cvss_integrity_impact = None
+                cvss_availability_impact = None
+                cvss_vector = None
+
+        except AttributeError as e:
+            print(e)
+            print(cve_id)
+            print(report.get("impact"))
+            print(summary)
+            print("------------")
             continue
 
-        if impact != {}:
-            baseMetricV2 = impact.get("baseMetricV2")
+        cve = {
+            "cveId": cve_id,
+            "publishedDate": pub_date,
+            "lastModifiedDate": last_mod_date,
+            "summary": summary,
+            "cvssBase": cvss_base,
+            "cvssImpact": cvss_impact,
+            "cvssExploit": cvss_exploit,
+            "cvssAccessVector": cvss_access_vector,
+            "cvssAccessComplexity": cvss_access_complexity,
+            "cvssAccessAuthentication": cvss_access_authentication,
+            "cvssConfidentialityImpact": cvss_confidentiality_impact,
+            "cvssIntegrityImpact": cvss_integrity_impact,
+            "cvssAvailabilityImpact": cvss_availability_impact,
+            "cvssVector": cvss_vector,
+            "cweId": cwe_id
+        }
+        cves.append(cve)
 
-            cvss_base = baseMetricV2.get("cvssV2").get("baseScore")
-            cvss_impact = baseMetricV2.get("impactScore")
-            cvss_exploit = baseMetricV2.get("exploitabilityScore")
-            cvss_access_vector = baseMetricV2.get("cvssV2").get("accessVector")
-            cvss_access_complexity = baseMetricV2.get("cvssV2").get("accessComplexity")
-            cvss_access_authentication = baseMetricV2.get("cvssV2").get("authentication")
-            cvss_confidentiality_impact = baseMetricV2.get("cvssV2").get("confidentialityImpact")
-            cvss_integrity_impact = baseMetricV2.get("cvssV2").get("integrityImpact")
-            cvss_availability_impact = baseMetricV2.get("cvssV2").get("availabilityImpact")
-            cvss_vector = baseMetricV2.get("cvssV2").get("vectorString")
-            cwe_id = report.get("cve").get("problemtype").get("problemtype_data")[0].get("description")[0].get("value")
-        else:
+    return cves
 
-            cvss_base = None
-            cvss_impact = None
-            cvss_exploit = None
-            cvss_access_vector = None
-            cvss_access_complexity = None
-            cvss_access_authentication = None
-            cvss_confidentiality_impact = None
-            cvss_integrity_impact = None
-            cvss_availability_impact = None
-            cvss_vector = None
 
-    except AttributeError as e:
-        print(e)
-        print(cve_id)
-        print(report.get("impact"))
-        print(summary)
-        print("------------")
-        continue
+print(extract_data_from_zip("nvd/nvdcve-1.1-2020.json.zip")[-1]["cveId"])
